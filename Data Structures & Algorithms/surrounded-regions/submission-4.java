6288e3e632d2e4e662823debdb05cd6fecde64b6
joaopinto339 <62262268+joaopinto339@users.noreply.github.com>
2026-04-08 21:45:32 +0100

    Add: surrounded-regions - submission-4

---
+++ Data Structures & Algorithms/surrounded-regions/submission-4.java
@@ -0,0 +1,51 @@
+class Solution {
+    private int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+    int ROWS, COLS;
+
+    public void solve(char[][] board) {
+        ROWS = board.length;
+        COLS = board[0].length;
+
+        for(int r = 0; r < ROWS; r++){
+            if(board[r][0] == 'O'){
+                dfs(r, 0, board);
+            }
+            if(board[r][COLS-1] == 'O'){
+                dfs(r, COLS-1, board);
+            }
+        }
+
+        for(int c = 0; c < COLS; c++){
+            if(board[0][c] == 'O'){
+                dfs(0, c, board);
+            }
+            if(board[ROWS-1][c] == 'O'){
+                dfs(ROWS-1, c, board);
+            }
+        }
+
+        for(int r = 0; r < ROWS; r++){
+            for(int c = 0; c < COLS; c++){
+                if(board[r][c] == 'O'){
+                    board[r][c] = 'X';
+                }
+                else if(board[r][c] == '#'){
+                    board[r][c] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(int r, int c, char[][] board){
+        board[r][c] = '#';
+
+        for(int[] dir : directions){
+            int nr = r + dir[0];
+            int nc = c + dir[1];
+            if(nr >= 0 && nr < ROWS && nc >= 0 && nc < COLS &&
+                board[nr][nc] == 'O'){
+                    dfs(nr, nc, board);
+                }
+        }
+    }
+}
